d0ed998e69c26630de0d39efd06031ce7b36a106
WilliamBHuynh <williamhuynh2@gmail.com>
2021-01-01 16:11:53 -0500

    Added conditional rendering of React components based on button state

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button, Dropdown, Form } from 'semantic-ui-react';
 import moment from 'moment';
 import './index.css'
 
-const App: React.FC = () => {
+const SnoozeComponents = (props: any) => {
 
   const isEarlierThanEndLimit = (timeValue: string, endLimit: string, lastValue: string) => {
     const timeValueIsEarlier = moment(timeValue, 'h:mmA').diff(moment(endLimit, 'h:mmA')) < 0;
@@ -16,16 +16,25 @@ const App: React.FC = () => {
   const endLimit = "11:59PM";
   const step = 30;
 
-  var snoozeTimeOptions = [];
+  const snoozeTimeOptions = [{ key: timeValue, text: timeValue, value: timeValue }];
   snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
 
   while (isEarlierThanEndLimit(timeValue, endLimit, lastValue)) {
     lastValue = timeValue;
-    console.log(timeValue, moment(timeValue, 'h:mmA').diff(moment(endLimit, 'h:mmA'), 'minutes'));
     timeValue = moment(timeValue, 'h:mmA').add(step, 'minutes').format('h:mmA');
     snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
   }
   snoozeTimeOptions.pop();
+
+  const [selectedSnoozeTime, setSelectedSnoozeTime] = useState(snoozeTimeOptions[16]);
+
+  const handleSnoozeTimeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
+    setSelectedSnoozeTime({
+      key: data.value,
+      text: data.value,
+      value: data.value
+    });
+  }
 
   const snoozeTypeOptions = [
     {
@@ -43,37 +52,100 @@ const App: React.FC = () => {
   const [selectedSnoozeType, setSelectedSnoozeType] = useState(snoozeTypeOptions[0]);
 
   const handleSnoozeTypeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
-    if (data) {
-      setSelectedSnoozeType({
-        key: data.value,
-        text: data.value,
-        value: data.value
-      });
-    }
+    setSelectedSnoozeType({
+      key: data.value,
+      text: data.value,
+      value: data.value
+    });
   }
 
+  const SnoozeInputComponents: React.FC = () => {
+    return (
+      <Form>
+        <Form.Field>
+          <Dropdown
+            fluid
+            selection
+            value={selectedSnoozeType.value}
+            options={snoozeTypeOptions}
+            onChange={handleSnoozeTypeChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Dropdown
+            fluid
+            selection
+            value={selectedSnoozeTime.value}
+            options={snoozeTimeOptions}
+            onChange={handleSnoozeTimeChange}
+          />
+        </Form.Field>
+      </Form>
+    );
+  }
+
+
+  const SnoozeOutputComponents: React.FC = () => {
+    return (
+      <div>
+        <p>Selected type: {selectedSnoozeType.value}</p>
+        <p>Selected time: {selectedSnoozeTime.value}</p>
+      </div>
+    );
+  }
+
+  const isSubmitted: boolean = props.isSubmitted;
+  if (!isSubmitted) {
+    return <SnoozeInputComponents />
+  }
+  return <SnoozeOutputComponents />;
+}
+
+const SubmitButton = (props: any) => {
   return (
-    <Form>
-      <h1 className="title">Sleep Calculator</h1>
-      <Form.Field>
-        <Dropdown
-          fluid
-          selection
-          value={selectedSnoozeType.value}
-          options={snoozeTypeOptions}
-          onChange={handleSnoozeTypeChange}
-        />
-      </Form.Field>
-      <Form.Field>
-        <Dropdown
-          fluid
-          selection
-          options={snoozeTimeOptions}
-        />
-      </Form.Field>
-      <Button primary>Calculate Snooze Times</Button>
-    </Form>
+    <Button primary onClick={props.onClick}>
+      Calculate Snooze Times
+    </Button>
   );
+}
+
+const RestartButton = (props: any) => {
+  return (
+    <Button primary onClick={props.onClick}>
+      Calculate Again
+    </Button>
+  );
+}
+
+class App extends React.Component<{}, { isSubmitted: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.handleSubmitClick = this.handleSubmitClick.bind(this);
+    this.handleRestartClick = this.handleRestartClick.bind(this);
+    this.state = { isSubmitted: false }
+  }
+
+  handleSubmitClick() {
+    this.setState({ isSubmitted: true });
+  }
+
+  handleRestartClick() {
+    this.setState({ isSubmitted: false });
+  }
+
+  render() {
+    const isSubmitted = this.state.isSubmitted;
+    let button = !isSubmitted ? <SubmitButton onClick={this.handleSubmitClick} /> :
+      <RestartButton onClick={this.handleRestartClick} />;
+
+    return (
+      <div>
+        <h1 className="title">Sleep Calculator</h1>
+        <SnoozeComponents isSubmitted={isSubmitted} />
+        {button}
+      </div>
+    );
+  }
 }
 
 export default App;
