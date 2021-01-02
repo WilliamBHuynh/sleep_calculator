473501d27d9def24df4c8b95779893e8fd9b073b
WilliamBHuynh <williamhuynh2@gmail.com>
2021-01-02 16:20:19 -0500

    Styled app

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,11 @@
-import React, { useState, SyntheticEvent } from 'react';
-import { Button, Dropdown, Form } from 'semantic-ui-react';
-import moment from 'moment';
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { SnoozeComponents } from './SnoozeComponents';
 import './index.css'
-
-const SnoozeComponents = (props: any) => {
-
-  const isEarlierThanEndLimit = (timeValue: string, endLimit: string, lastValue: string) => {
-    const timeValueIsEarlier = moment(timeValue, 'h:mmA').diff(moment(endLimit, 'h:mmA')) < 0;
-    const timeValueIsLaterThanLastValue = lastValue === "" ? true : moment(lastValue, 'h:mmA').diff(moment(timeValue, 'h:mmA')) < 0;
-    return timeValueIsEarlier && timeValueIsLaterThanLastValue;
-  }
-
-  let timeValue = "12:00AM";
-  let lastValue = "";
-  const endLimit = "11:59PM";
-  const step = 30;
-
-  const snoozeTimeOptions = [{ key: timeValue, text: timeValue, value: timeValue }];
-  snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
-
-  while (isEarlierThanEndLimit(timeValue, endLimit, lastValue)) {
-    lastValue = timeValue;
-    timeValue = moment(timeValue, 'h:mmA').add(step, 'minutes').format('h:mmA');
-    snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
-  }
-  snoozeTimeOptions.pop();
-
-  const [selectedSnoozeTime, setSelectedSnoozeTime] = useState(snoozeTimeOptions[16]);
-
-  const handleSnoozeTimeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
-    setSelectedSnoozeTime({
-      key: data.value,
-      text: data.value,
-      value: data.value
-    });
-  }
-
-  const snoozeTypeOptions = [
-    {
-      key: "I need to wake up by...",
-      text: "I need to wake up by...",
-      value: "I need to wake up by..."
-    },
-    {
-      key: "I plan to sleep by...",
-      text: "I plan to sleep by...",
-      value: "I plan to sleep by..."
-    }
-  ];
-
-  const [selectedSnoozeType, setSelectedSnoozeType] = useState(snoozeTypeOptions[0]);
-
-  const handleSnoozeTypeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
-    setSelectedSnoozeType({
-      key: data.value,
-      text: data.value,
-      value: data.value
-    });
-  }
-
-  const SnoozeInputComponents: React.FC = () => {
-    return (
-      <Form>
-        <Form.Field>
-          <Dropdown
-            fluid
-            selection
-            value={selectedSnoozeType.value}
-            options={snoozeTypeOptions}
-            onChange={handleSnoozeTypeChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Dropdown
-            fluid
-            selection
-            value={selectedSnoozeTime.value}
-            options={snoozeTimeOptions}
-            onChange={handleSnoozeTimeChange}
-          />
-        </Form.Field>
-      </Form>
-    );
-  }
-
-
-  const SnoozeOutputComponents: React.FC = () => {
-    return (
-      <div>
-        <p>Selected type: {selectedSnoozeType.value}</p>
-        <p>Selected time: {selectedSnoozeTime.value}</p>
-      </div>
-    );
-  }
-
-  const isSubmitted: boolean = props.isSubmitted;
-  if (!isSubmitted) {
-    return <SnoozeInputComponents />
-  }
-  return <SnoozeOutputComponents />;
-}
 
 const SubmitButton = (props: any) => {
   return (
-    <Button primary onClick={props.onClick}>
+    <Button color='yellow' onClick={props.onClick}>
       Calculate Snooze Times
     </Button>
   );
@@ -111,7 +13,7 @@ const SubmitButton = (props: any) => {
 
 const RestartButton = (props: any) => {
   return (
-    <Button primary onClick={props.onClick}>
+    <Button color='yellow' onClick={props.onClick}>
       Calculate Again
     </Button>
   );
@@ -140,9 +42,16 @@ class App extends React.Component<{}, { isSubmitted: boolean }> {
 
     return (
       <div>
-        <h1 className="title">Sleep Calculator</h1>
-        <SnoozeComponents isSubmitted={isSubmitted} />
-        {button}
+        <div>
+          <img src='moon.gif' alt='Gif of a cartoon drawn moon' />
+        </div>
+        <div>
+          <h1 className="title">Sleep Calculator</h1>
+          <SnoozeComponents isSubmitted={isSubmitted} />
+        </div>
+        <div className='button'>
+          {button}
+        </div>
       </div>
     );
   }
---
+++ src/SnoozeComponents.tsx
@@ -0,0 +1,175 @@
+import React, { useState, SyntheticEvent } from 'react';
+import { Dropdown, Grid } from 'semantic-ui-react';
+import moment from 'moment';
+import './index.css'
+
+export const SnoozeComponents = (props: any) => {
+
+    const isEarlierThanEndLimit = (timeValue: string, endLimit: string, lastValue: string) => {
+        const timeValueIsEarlier = moment(timeValue, 'h:mmA').diff(moment(endLimit, 'h:mmA')) < 0;
+        const timeValueIsLaterThanLastValue = lastValue === "" ? true : moment(lastValue, 'h:mmA').diff(moment(timeValue, 'h:mmA')) < 0;
+        return timeValueIsEarlier && timeValueIsLaterThanLastValue;
+    }
+
+    let timeValue = "12:00AM";
+    let lastValue = "";
+    const endLimit = "11:59PM";
+    const step = 30;
+
+    const snoozeTimeOptions = [{ key: timeValue, text: timeValue, value: timeValue }];
+    snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
+
+    while (isEarlierThanEndLimit(timeValue, endLimit, lastValue)) {
+        lastValue = timeValue;
+        timeValue = moment(timeValue, 'h:mmA').add(step, 'minutes').format('h:mmA');
+        snoozeTimeOptions.push({ key: timeValue, text: timeValue, value: timeValue });
+    }
+    snoozeTimeOptions.pop();
+
+    const [selectedSnoozeTime, setSelectedSnoozeTime] = useState(snoozeTimeOptions[17]);
+
+    const handleSnoozeTimeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
+        setSelectedSnoozeTime({
+            key: data.value,
+            text: data.value,
+            value: data.value
+        });
+    }
+
+    const snoozeTypeOptions = [
+        {
+            key: "I need to wake up by...",
+            text: "I need to wake up by...",
+            value: "I need to wake up by..."
+        },
+        {
+            key: "I plan to sleep by...",
+            text: "I plan to sleep by...",
+            value: "I plan to sleep by..."
+        }
+    ];
+
+    const [selectedSnoozeType, setSelectedSnoozeType] = useState(snoozeTypeOptions[0]);
+
+    const handleSnoozeTypeChange = (event: SyntheticEvent<HTMLElement>, data: any) => {
+        setSelectedSnoozeType({
+            key: data.value,
+            text: data.value,
+            value: data.value
+        });
+    }
+
+    const SnoozeInputText: React.FC = () => {
+        return (
+            <label>
+                Find out when you should
+                <span className='highlight'> sleep </span>
+                    or
+                <span className='highlight'> wake up </span>
+                    by to get that good night’s sleep!
+            </label>
+        );
+    }
+
+    const SnoozeInputComponents: React.FC = () => {
+        return (
+            <div>
+                <div className='textPadding'><SnoozeInputText /></div>
+                <div className='dropdown'>
+                    <Dropdown
+                        fluid
+                        selection
+                        value={selectedSnoozeType.value}
+                        options={snoozeTypeOptions}
+                        onChange={handleSnoozeTypeChange}
+                    />
+                </div>
+                <div className='dropdown'>
+                    <Dropdown
+                        fluid
+                        selection
+                        value={selectedSnoozeTime.value}
+                        options={snoozeTimeOptions}
+                        onChange={handleSnoozeTimeChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+    const SnoozeOutputText: React.FC = () => {
+        if (selectedSnoozeType.value.includes('wake')) {
+            return (
+                <label>
+                    To wake up refreshed at
+                    <span className='highlight'> {selectedSnoozeTime.value}</span>
+                    , I need to fall asleep at one of these times:
+                </label>
+            );
+        }
+        return (
+            <label>
+                To sleep at
+                <span className='highlight'> {selectedSnoozeTime.value}</span>
+                , you should wake up at these times to feel refreshed:
+            </label>
+        );
+    }
+
+    const SnoozeOutputComponents: React.FC = () => {
+        // Calculate snooze times based on inputted time
+        const snoozeTimes = [];
+        const sleepCycleStep = 90;
+        const tempSelectedTime = moment(selectedSnoozeTime.value, 'h:mmA');
+
+        for (let i = 1; i < 7; i++) {
+            if (selectedSnoozeType.value.includes('wake')) {
+                // calculate wake up times
+                snoozeTimes.push(moment(tempSelectedTime, 'h:mmA').subtract(sleepCycleStep * (i), "minutes").format('h:mmA'));
+            } else {
+                // calculate sleep by times
+                snoozeTimes.push(moment(tempSelectedTime, 'h:mmA').add(sleepCycleStep * (i), "minutes").format('h:mmA'));
+            }
+        }
+
+        // display grid of resulting snooze times
+        return (
+            <div>
+                <div className='textPadding'><SnoozeOutputText /></div>
+                <div className='grid'>
+                    <Grid columns={3}>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <label className='highlight'>{snoozeTimes[5]}</label>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <label className='highlight'>{snoozeTimes[4]}</label>
+                            </Grid.Column>
+                            <Grid.Column>
+                                {snoozeTimes[3]}
+                            </Grid.Column>
+                        </Grid.Row>
+
+                        <Grid.Row>
+                            <Grid.Column>
+                                {snoozeTimes[2]}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {snoozeTimes[1]}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {snoozeTimes[0]}
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
+
+    const isSubmitted: boolean = props.isSubmitted;
+    if (!isSubmitted) {
+        return <SnoozeInputComponents />
+    }
+    return <SnoozeOutputComponents />;
+}
